00b21bd41388b5f792ad74c51b9b2b451131ae3e
unknown <baptiste.dupy@STUD-VDOC-057.formation.enjmin.fr>
2015-02-10 19:50:49 +0100

    SwitchRenderers

---
+++ Actions/Basic/GPActionSwitchRenderers.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ActionTool
+{
+    [GPActionAlias("Switchs/Renderers")]
+    public class GPActionSwitchRenderer : GPAction
+    {
+        public List<Renderer> _disableRenderers;
+        public List<Renderer> _enableRenderers;
+        protected override void OnTrigger()
+        {
+            foreach (Renderer rd in _disableRenderers)
+                rd.enabled = false;
+
+            foreach (Renderer rd in _enableRenderers)
+                rd.enabled = true;
+        }
+    }
+
+}
