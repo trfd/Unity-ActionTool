39f5ea97349468eb01c7254e43904473a4e410da
unknown <baptiste.dupy@STUD-VDOC-057.formation.enjmin.fr>
2015-02-11 15:45:44 +0100

    Shuriken Actions

---
+++ Actions/Shuriken/GPActionPlayParticles.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ActionTool
+{
+    [GPActionAlias("Shuriken/Play Particles")]
+    public class GPActionPlayParticles : GPAction
+    {
+        #region Public Members
+
+        public List<ParticleSystem> _systems;
+
+        #endregion 
+
+        protected override void OnTrigger()
+        {
+            foreach (ParticleSystem syst in _systems)
+                syst.Play(false);
+
+            End();
+        }
+    }
+
+}
---
+++ Actions/Shuriken/GPActionStopParticles.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ActionTool
+{
+    [GPActionAlias("Shuriken/Stop Particles")]
+    public class GPActionStopParticles : GPAction
+    {
+        #region Public Members
+
+        public List<ParticleSystem> _systems;
+
+        #endregion
+
+        protected override void OnTrigger()
+        {
+            foreach (ParticleSystem syst in _systems)
+                syst.Stop(false);
+
+            End();
+        }
+    }
+
+}
