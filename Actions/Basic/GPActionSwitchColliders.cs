bbab7df3f67460a36549ad689f33880f68d0e39c
unknown <baptiste.dupy@STUD-VDOC-057.formation.enjmin.fr>
2015-02-12 12:41:20 +0100

    GPActionVectorAnimation & GPActionSwitchCollider

---
+++ Actions/Basic/GPActionSwitchColliders.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ActionTool
+{
+    [GPActionAlias("Switchs/Colliders")]
+    public class GPActionSwitchColliders : GPAction
+    {
+        public List<Collider> _disableColliders;
+        public List<Collider> _enableColliders;
+        protected override void OnTrigger()
+        {
+            foreach (Collider rd in _disableColliders)
+                rd.enabled = false;
+
+            foreach (Collider rd in _enableColliders)
+                rd.enabled = true;
+        }
+    }
+
+}
